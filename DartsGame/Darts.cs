4e95b5bfd74558dfb98613aa147a19a153398a37
Семашко Евгений <ezarioexp@gmail.com>
2021-04-07 19:27:09 +0300

    Добавьте файлы проекта.

---
+++ DartsGame/Darts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        /// <summary>
+        /// Calculates the earned points in a single toss of a Darts game.
+        /// </summary>
+        /// <param name="x">x-coordinate of dart.</param>
+        /// <param name="y">y-coordinate of dart.</param>
+        /// <returns>The earned points.</returns>
+        public static int GetScore(double x, double y)
+        {
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 100 && (Math.Pow(x, 2) + Math.Pow(y, 2)) > 25)
+            {
+                return 1;
+            }
+            else if ((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 25 && (Math.Pow(x, 2) + Math.Pow(y, 2)) > 1)
+            {
+                return 5;
+            }
+            else if ((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1)
+            {
+                return 10;
+            }
+            else
+            {
+                return 0;
+            }
+        } 
+    }
+}
